c72a0299db99aeb0b5665569141d587b0eae71ec
George.Flanagin <gflanagin@richmond.edu>
2022-06-27 10:28:43 -0400

    Move all the input to the command line; a little cleanup.

--- ssh_bot.py
+++ ssh_bot.py
@@ -10,79 +10,79 @@ if sys.version_info < min_py:
     print(f"This program requires at least Python {min_py[0]}.{min_py[1]}")
     sys.exit(os.EX_SOFTWARE)
 
+import argparse
+import getpass
 import socket
 import sched
 import time
 
 import paramiko
 
-script, job_file = sys.argv
 
-host = "server.richmond.edu"
-ssh_username = "netid"
-ssh_password = "xxxxxxxxxxxxxxxxxxxxx"
-
-def ssh_bot (hostname:str, username:str, password:str, commands:list=[]):
+def ssh_bot (myargs:argparse.Namespace) -> int:
     """
     Execute a few commands using Paramiko.
     """
+    commands = gaussian_job(myargs)
+
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
     ssh.load_system_host_keys()
+
     result_flag = True
     try:
         ssh.connect (
-            hostname=host,
-            username=ssh_username,
-            password=ssh_password)
-
-        # Added for loop here but didn't work
-        print(commands)
-        stdin, stdout, stderr = ssh.exec_command(commands, timeout=10) 
-        
-        ssh_output = stdout.readlines()
-        ssh_err = stderr.read()
-        if ssh_err:
-            print ("Problem occured while running command:" + commands + "Error:" + ssh_err)
-            result_flag = False
-        else:
-            print(''.join(ssh_output))
-            result_flag = False
-        ssh.close()
+            hostname=myargs.host,
+            username=myargs.netid,
+            password=myargs.password
+            )
 
     except socket.timeout as e:
         print ("command timed out.", commands)
         ssh.client.close()
         result_flag = False
+
     except paramiko.SSHException:
         print("Falid to execute the command!", commands)
         ssh.client.close()
         result_flag = False
 
+    except Exception as e:
+        print(f"Unknown exception: {e}")
+        return os.EX_IOERR
 
-def gaussian_job (job_file:str) -> list:
-
-    project_folder = input ("Name of project folder:> ") or None
-    sub_folder_1 = input ("Name of 1st subfolder folder:> ") or None
-    sub_folder_2 = input ("Name of 2nd subfolder folder:> ") or None
-    jobs = open(job_file, 'r')
-
-    commands = []    
-
-    for job in jobs:
-        if project_folder == None:
-            commands.append(f'cd work && qg16 {job}')
-        if project_folder != None:
-            if sub_folder_1 == None:
-                commands.append(f'cd work && cd {project_folder} && qg16 {job}')
-            if (sub_folder_1 != None) and (sub_folder_2 == None):
-                commands.append(f'cd work && cd {project_folder} && cd {sub_folder_1} && qg16 {job}')
-            if (sub_folder_1 != None) and (sub_folder_2 != None):
-                commands.append(f'cd work && cd {project_folder} && cd {sub_folder_1} && cd {sub_folder_2} && qg16 {job}')
-    return commands
+    for command in commands:
+        stdin, stdout, stderr = ssh.exec_command(commands, timeout=10) 
+        ssh_output = stdout.readlines()
+        if (ssh_err := stderr.read()):
+            print (f"Problem occured while running command: {command}\nError: {ssh_err}")
+            return os.EX_IOERR
+    else:
+        ssh.close()
 
 
-commands = gaussian_job(job_file)
 
-#ssh_bot (host, ssh_username, ssh_password, commands)
+def gaussian_job (myargs:argparse.Namespace) -> list:
+
+    lines = open(myargs.job_file).split("\n")
+    return tuple( f"cd {myargs.dir} && qg16 {line}" for line in lines )
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog='gaussian_job',
+        description='gaussian_job: Launch gaussian job on the cluster')
+
+    parser.add_argument('-u', '--netid', type=str, default=getpass.getuser(),
+        help="User id for submitting the job. Defaults to the user running this program.")
+    parser.add_argument('-p', '--password', type=str, required=True,
+        help="Password for the user id.")
+    parser.add_argument('-h', '--host', type=str, default=socket.gethostname(),
+        help="Name of CPU where job will be submitted. Defaults to *this* CPU.")
+    parser.add_argument('-d', '--dir', type=str, default="",
+        help="Name of directory under /work that contains the data.")
+
+    parser.add_argument('job_file', type=str, required=True,
+        help="Name of the file containing the job to be submitted.")
+
+    sys.exit(ssh_bot(parser.parse_args()))
+
